74377afa908fb61b100e17237ddf041224d44fdd
frgauthi <fr.gauthi@gmail.com>
2014-10-05 22:34:33 -0600

    Final Version of Assignment 1

--- dictionary.cpp
+++ dictionary.cpp
@@ -1,5 +1,9 @@
-/*A program to create a dictionary from a text file and provide frequency analysis
-  Frankie Gauthier - 4213091 - cs2010 - Assign1*/
+//########################################################################################//
+//##											##//
+//##  A program to create a dictionary from a text file and provide frequency analysis	##//
+//##  		     Frankie Gauthier - 4213091 - cs2010 - Assign1*/			##//
+//##											##//
+//########################################################################################//
 
 #include <cstdio>
 #include <cctype>
@@ -7,31 +11,63 @@
 
 using namespace std;
 
-//prototype functions
+// Function Prototypes
 void Dump(void);
 int strLength(int);
 bool wordCompare(int,int);
+bool wordExists(int);
+void clearRow(int);
+void addWord();
+int locateWord(int);
 
 
-const int maxRows = 10;
-const int maxLength = 132;
+const int maxRows = 10;			// maximum number of words in the dictionary
+const int maxLength = 20;		// max number of characters in a word
 char dictionary[maxRows][maxLength]={0}; // init the char array with all zero's
+int frequency[10] = {0};		// create int array to track the frequency of words
 int ptr = 0; 				// init the row pointer as 0
+
+
+// clears a row number that is passed to the function
+void clearRow(int row){
+	int i =0;
+	while(dictionary[row][i] != 0){
+		dictionary[row][i] = 0;
+		i++;
+	}
+}
+
+
+// returns the location of the word passed to the function based on its row number
+// if the word isnt found returns 0;
+int locateWord(int row){
+	for(int i = 0; i < ptr; i++){
+        	if (wordCompare(row,i)) return i;
+        }
+        return 0;
+
+}
+
+
+// searches the dictionary for the word passed into the function by its row number
+bool wordExists(int row){
+	for(int i = 0; i < ptr; i++){
+		if (wordCompare(row,i)) return true;
+	}
+	return false;
+}
+
  
 // compare two rows to determine if the words are the same
 bool wordCompare(int row1, int row2){
-	
-	int col = 0;
-	if(strLength(row1) == strLength(row2))
-	{
-		while(dictionary[row1][col] != 0)
-		{
-			if(dictionary[row1][col] =! dictionary[row2][col]) return false; 
-		}
-		return true;
 		
+	if (strLength(row1) != strLength(row2)) return false;
+	for( int i = 0; i<strLength(row1); i++)
+	{
+		if (dictionary[row1][i] != dictionary[row2][i]) return false;
 	}
-	else return false;
+		
+return true;
 }
 
 // strLength determines the length of a particular row in the dictionary
@@ -45,6 +81,7 @@ return count;
 }
 
 // Dump takes a 2d array of chars and prints it with each row on a seperate line
+// and prints the frequency next to the word
 void Dump(){
 	
 	for(int i = 0; i< ptr; i++)
@@ -53,8 +90,9 @@ void Dump(){
 		{
 			if(dictionary[i][j] == 0) break;
 			cout << dictionary[i][j];
+			
 		}
-	cout << endl;
+	cout << "\t\t\t" << frequency[i]  <<  endl;
 	}
 	 
 }
@@ -67,30 +105,56 @@ char ch;
 int newWord= 0;
 int ltrCount = 0;
  
-
+// while there is input valid input put words into the dictionary
 while( cin.good() )
 {
- ch = cin.get();
+
+// if there are too many words for the array warn user and break out of the loop
+if(ptr > maxRows-1){	
+cout << endl << "* Too many words in the file.. dictionary contains the first 10 words" << endl;
+break;
+}
+
+ ch = tolower(cin.get()); // convert the characters that are read into lowercase
+
  if ( isalpha(ch) )  /* function test is ch  is A-Z, a-z */
     {
-       	dictionary[ptr][ltrCount] = ch;
-       	newWord = 1;
-	ltrCount += 1; // increment the column pointer
-
+		if(ltrCount < maxLength){
+       		dictionary[ptr][ltrCount] = ch;  	// while there is room to input more chars add next char
+       		newWord = 1;				// to dictionary
+		ltrCount += 1; 
+		}
+		
+			
+		
 	}
+	
  else
        if (newWord) 	
           {
-		ptr += 1; // set ptr location to the next word in the array
-		ltrCount = 0; // set col pointer for loop back to 0 to start a new word
-              	newWord = 0; // 
-          }
+		if (ltrCount >= maxLength) cout << endl <<  "* Some words have exceeded the maximum length and have been truncated!";
+		
+		if(wordExists(ptr)){
+			frequency[locateWord(ptr)]++;
+			clearRow(ptr);				// clear the row and reset to input next word
+			ltrCount =0;
+			newWord =0;
+		}
+		else{
+			frequency[ptr]++;	
+			ptr += 1; 			//increase the pointer and reset to read next word
+			ltrCount = 0; 		
+              		newWord = 0; 
+        	}  
+	}
 
 }
- Dump();   // dump dictionary
- cout << "length of first string : " << strLength(0) << endl;
- cout << "length of second string: " << strLength(1) << endl;
- cout << wordCompare(0,0) << endl;
- return 0;
+
+ cout << endl  << "Word\t\t    Frequency" << endl << "-----------------------------" << endl;
+	 
+ Dump();   // dump dictionary 
+ 
+
+return 0;
 }
  
